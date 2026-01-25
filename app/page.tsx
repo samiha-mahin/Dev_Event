cd726c1f06170516b60ebd0dac3e2db4cc251677
samiha-mahin <samihamahin36@gmail.com>
2026-01-25 15:08:07 +0600

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 const Home = () => {
   return (
-    <h1>Hello World This is 2016</h1>
-  )
-}
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
